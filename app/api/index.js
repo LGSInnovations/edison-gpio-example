06dab32e8a347c749ed1d02748dda9f1d85c306e
Parker Lusk <parkerclusk@gmail.com>
2015-07-29 10:31:55 -0600

    fixed centering and resizing of GPIO block image

--- app/api/index.js
+++ app/api/index.js
@@ -5,14 +5,14 @@ var gpioStates  = [{pin:31, value:0}];
 
 // get current state of gpio
 router.get('/gpio', function (req, res) {
-	res.send(gpioStates);
+    res.send(gpioStates);
 
 });
 
 //set current state of individual pin
 router.post('/gpio/:pin', function (req, res) {
-	var pin = req.params.pin;
-	res.send(pin);
+    var pin = req.params.pin;
+    res.send(pin);
 
 });
 
